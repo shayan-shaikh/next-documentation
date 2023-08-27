c61d2a5d1f73f101d3bbfa147a51def677c1006a
shayan-shaikh <65915426+shayan-shaikh@users.noreply.github.com>
2023-08-27 12:25:45 +0530

    image takes you to repo

--- theme.config.jsx
+++ theme.config.jsx
@@ -1,6 +1,6 @@
 export default {
     logo: <span>Documentation</span>,
     project: {
-        link: 'https://github.com/shuding/nextra'
+        link: 'https://github.com/shayan-shaikh/next-documentation'
     }
 }
